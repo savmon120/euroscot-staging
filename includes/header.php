a0e7893958587f39edb5a5a12cad717a70b8a2ac
Sav Monzac <savmonzac@Savs-MacBook-Pro.local>
2025-11-22 21:23:20 +0000

    Add responsive navbar with z-index fix

--- includes/header.php
+++ includes/header.php
@@ -14,6 +14,10 @@ $currentPage = $page ?? 'home';
   </div>
 
   <nav class="eus-nav-wrapper">
+    <!-- Hamburger toggle --> 
+     <div class="menu-toggle" id="mobile-menu"> 
+      ☰ 
+    </div>
     <ul class="eus-nav-links">
       <?php foreach ($navLinks as $slug => $label): ?>
         <li>
